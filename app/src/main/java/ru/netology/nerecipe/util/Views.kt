8e6495c8e0b258aeea1ba81088cbebd2d61d6291
apexnt <apexnt@yandex.ru>
2022-08-31 16:21:04 +0500

    commit

---
+++ app/src/main/java/ru/netology/nerecipe/util/Views.kt
@@ -0,0 +1,34 @@
+package ru.netology.nerecipe.util
+
+import android.content.Context
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.inputmethod.InputMethodManager
+
+
+fun View.focusAndShowKeyboard() {
+    fun View.showTheKeyboardNow() {
+        if (isFocused) {
+            post {
+                val imm =
+                    context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+            }
+        }
+    }
+
+    requestFocus()
+    if (hasWindowFocus()) {
+        showTheKeyboardNow()
+    } else {
+        viewTreeObserver.addOnWindowFocusChangeListener(
+            object : ViewTreeObserver.OnWindowFocusChangeListener {
+                override fun onWindowFocusChanged(hasFocus: Boolean) {
+                    if (hasFocus) {
+                        this@focusAndShowKeyboard.showTheKeyboardNow()
+                        viewTreeObserver.removeOnWindowFocusChangeListener(this)
+                    }
+                }
+            })
+    }
+}
